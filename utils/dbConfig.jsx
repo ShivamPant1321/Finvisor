4cb1f93c912042fd1b8731fe0114d6006ede7c8c
ShivamPant1321 <160627655+ShivamPant1321@users.noreply.github.com>
2025-05-14 10:46:25 +0530

    added

--- utils/dbConfig.jsx
+++ utils/dbConfig.jsx
@@ -1,12 +1,14 @@
 import { neon } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
 import * as schema from "./schema";
-import { cwd } from "node:process";
 import { config } from "dotenv"; // Import dotenv
 
 // Load environment variables from .env file
-config({ path: `${cwd()}/.env` });
+// Using process.cwd() directly instead of calling unimported cwd function
+config({ path: `${process.cwd()}/.env` });
 
-const sql = neon(process.env.NEXT_PUBLIC_DATABASE_URL);
+const dbUrl = process.env.NEXT_PUBLIC_DATABASE_URL;
+
+const sql = neon(dbUrl);
 
 export const db = drizzle(sql, { schema });
